8a7061bdd83e0b97a489e815684d9ed33fa254a1
JJong <entere7761@gmail.com>
2023-05-21 19:31:27 +0900

    [FEAT] 전체 카테고리 조회 기능 구현

---
+++ src/app/Category/categoryRoute.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+    const category = require('./categoryController');
+    const jwtMiddleware = require('../../../config/jwtMiddleware');
+
+    // 전체 카테고리 기능
+    app.get('/categories', category.getCategories);
+};
--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -2,5 +2,7 @@ module.exports = function(app){
     const user = require('./userController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
+    // 유저 생성 기능
     app.post('/users/signup', user.signup);
+
 };

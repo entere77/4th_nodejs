e26110a414715bbc1029b031c1c351cedeb14079
JJong <entere7761@gmail.com>
2023-05-29 20:10:44 +0900

    [ADD] mypageRoute.js URI

--- src/app/Mypage/mypageRoute.js
+++ src/app/Mypage/mypageRoute.js
@@ -2,4 +2,6 @@ module.exports = function(app){
     const mypage = require('./mypageController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
     
+    // mypage 조회 기능
+    app.get('/mypage', mypage.getmypage);    
 };

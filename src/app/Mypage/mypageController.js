9f3d9f2078d3199aa859fc61751996ee9f04a06c
JJong <entere7761@gmail.com>
2023-06-01 00:28:02 +0900

    [ADD] mypageController.js 코드 작성

--- src/app/Mypage/mypageController.js
+++ src/app/Mypage/mypageController.js
@@ -6,3 +6,14 @@ const {response, errResponse} = require("../../../config/response");
 
 const regexEmail = require("regex-email");
 const {emit} = require("nodemon");
+
+/**
+ * API No. 1
+ * API Name : 마이페이지 조회 기능 API
+ * [GET] /mypage
+ */
+exports.getMypage = async function (req, res) {
+    const mypage = await mypageProvider.mypageList();
+
+    return res.send(response(baseResponse.SUCCESS, mypage));
+};

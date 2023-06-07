b29a22578efe93d04290347368987df48977993b
JJong <entere7761@gmail.com>
2023-06-07 17:59:01 +0900

    [ADD] mypageController.js 관심목록 조회 기능 추가

--- src/app/Mypage/mypageRoute.js
+++ src/app/Mypage/mypageRoute.js
@@ -3,8 +3,8 @@ module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
     
     // mypage 조회 기능
-    app.get('/mypage', mypage.getmypage);
+    app.get('/mypage', mypage.getMypage);
     
     // mypage 관심목록 조회 기능
-    app.get('/mypage/heart', mypage.getheart);
+    app.get('/mypage/heart', mypage.getHeart);
 };

3ef1d5bc36d6e47c9079738c5544d69651b5be56
JJong <entere7761@gmail.com>
2023-06-15 17:34:17 +0900

    [ADD] chatProvider.js 코드 추가

---
+++ src/app/Chat/chatProvider.js
@@ -0,0 +1,14 @@
+const { pool } = require("../../../config/database");
+const { logger } = require("../../../config/winston");
+
+const chatDao = require("./chatDao");
+
+// Provider: Read 비즈니스 로직 처리
+
+exports.allChatsList = async function (email) {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const chatsList = chatDao.getChats(connection);
+    connection.release();
+
+    return chatsList;
+};

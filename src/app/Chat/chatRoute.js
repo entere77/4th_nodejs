dc03cbba53f2719b20a436966a6462bc32792c8d
JJong <entere7761@gmail.com>
2023-06-18 01:03:22 +0900

    [ADD] chatRoute.js에 특정 채팅 기록 선택 경로 추가

--- src/app/Chat/chatRoute.js
+++ src/app/Chat/chatRoute.js
@@ -2,6 +2,9 @@ module.exports = function(app){
     const chatRoute = require('./chatRouteController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
-    // 전체 카테고리 조회 기능
+    // 전체 채팅 기록 조회하기
     app.get('/chat', chat.getChat);
+
+    // 특정 채팅 기록 선택하기
+    app.get('/chat/:chatingId', chat.getChatBySelect);
 };

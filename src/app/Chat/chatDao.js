aaf2e1a90129db65b7d50962b99f430aa9eb76ee
JJong <entere7761@gmail.com>
2023-06-21 18:29:39 +0900

    [ADD] chatDao.js 특정 채팅 기록 조회 기능 코드 추가

--- src/app/Chat/chatDao.js
+++ src/app/Chat/chatDao.js
@@ -8,8 +8,21 @@ async function getChats(connection) {
     );
   
     return allChatsInfoRow[0];
-  }
+}
+
+// 특정 채팅 기록 조회하기
+async function getChatBySelect(connection) {
+  const selectedChatInfoQuery =`
+        select * from chats where chat_id = ?;
+  `
+  const selectedChatInfoRow = await connection.query(
+    selectedChatInfoQuery
+  );
+
+  return selectedChatInfoRow[0];
+}
   
   module.exports = {
     getChats,
+    getChatBySelect,
   };
